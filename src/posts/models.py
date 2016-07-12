6f4680eb5285c9e008649c58ee0354bd9ecf36f0
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-07-12 00:14:31 -0500

    Defining the models for the Posts app

--- src/posts/models.py
+++ src/posts/models.py
@@ -3,3 +3,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+
+    ''' Text fiels are lot bigger than char fields
+    Username, title are for char fields
+    '''
+
+    title = models.CharField()
+    content = models.TextField()
+
+    ''' Auto_now        --> Every time it is something is saved into database it will update it
+        Auto_now_add    -->  Whenever it is added into the database initially not whenever it is saved.
+                             So its one time. So saved and set.   '''
+
+    updated = models.DateTimeField(auto_now=True,auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False,auto_now_add=True)
+
+
+    def __unicode__(self):
+        return self.title
