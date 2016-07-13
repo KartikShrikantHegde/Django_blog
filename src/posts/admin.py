0b18858a8f71baefd2c84a23050b68cf0d1a0116
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-07-12 20:34:50 -0500

    Registering posts to admin

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# .models - since both models and admin are in same directory
+from .models import Post
+
+admin.site.register(Post)
