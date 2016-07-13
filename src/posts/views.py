1d7432369d36c33cb8ee3a79e59b6141d3f4e1db
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-07-12 22:18:26 -0500

    Creating a view for the Posts

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+# This is a function based view, so it takes in request and returns a response
+
+def posts_home(request):
+    return HttpResponse("<h1>Hello<h1>")
