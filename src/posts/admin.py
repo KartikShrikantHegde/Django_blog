56897f4525e0b4d175ab089b21ea2b11d4f89334
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-07-12 21:19:07 -0500

    Creating a customized model for Polls in admin

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -5,4 +5,19 @@ from django.contrib import admin
 # .models - since both models and admin are in same directory
 from .models import Post
 
-admin.site.register(Post)
+# Creating an customized admin for the Polls app
+# ModelAdmin is a class in Model
+
+class PostModelAdmin (admin.ModelAdmin):
+    # list_display is a django-admin keyword. other attributes match with model attributes
+    list_display = ["title","updated","timestamp"]
+    list_display_links = ["updated"]
+    list_filter = ["updated","timestamp"]
+    list_editable = ["title"]
+    search_fields = ["title","content"]
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostModelAdmin)
