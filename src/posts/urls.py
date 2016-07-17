b68348a074c5dea7d14cd758f5137dc6bbf6203e
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-07-16 23:54:54 -0400

    Adding simpler method of views

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from . import views
+from .views import (
+post_list,
+post_create,
+post_detail,
+post_update,
+post_delete,
+)
 
 
 '''
@@ -14,12 +20,11 @@ here even though its create url, detail function will be called.
 '''
 
 urlpatterns = [
-    url(r'^$',   "posts.views.post_list"),
-    url(r'^create/$', "posts.views.post_create"),
-    url(r'^detail/$', "posts.views.post_detail"),
-    url(r'^update/$', "posts.views.post_update"),
-    url(r'^delete/$', "posts.views.post_delete"),
-
+    url(r'^$',  post_list),
+    url(r'^create/$', post_create),
+    url(r'^detail/$', post_detail),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
 
 ]
 
