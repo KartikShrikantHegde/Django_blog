9e9d28f27313561b525b2bb26316069259935c55
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-07-17 23:19:14 -0400

    Adding named url

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -22,7 +22,7 @@ here even though its create url, detail function will be called.
 urlpatterns = [
     url(r'^$',  post_list),
     url(r'^create/$', post_create),
-    url(r'^detail/$', post_detail),
+    url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
     url(r'^update/$', post_update),
     url(r'^delete/$', post_delete),
 
