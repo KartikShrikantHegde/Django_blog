e993d457c6aaafa4992b39036d8f918f47ac1c66
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-07-19 20:21:15 -0400

    Adding paths of static folders

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -123,3 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    #'/var/www/static/',
+]
+
+# this is the location above src in django blog - which is a virtual environment.
+
+STATIC_ROOT = os.path.join(os.path.dirname (BASE_DIR),"static_cdn")
