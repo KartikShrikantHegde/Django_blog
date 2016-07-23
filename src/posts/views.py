58481e68055caa5edbe2904ccc3a76422ede6b4e
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-07-23 11:06:26 -0400

    Adding permissions for admin and staff

--- src/posts/views.py
+++ src/posts/views.py
@@ -2,7 +2,7 @@ from urllib import quote_plus
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
@@ -14,6 +14,10 @@ from .models import Post
 # render takes in a request
 
 def post_create(request):
+    # Not to allow other than staff and admin to create posts.
+
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
 
     # This is the best method as it directly references the form model and the required fields as well.
 
@@ -97,6 +101,8 @@ def post_list(request):
 
 
 def post_update(request, id = None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
 
     instance = get_object_or_404(Post, id=id)
     form = PostForm(request.POST or None, request.FILES or None,instance = instance)
@@ -120,6 +126,8 @@ def post_update(request, id = None):
     return render(request, "post_form.html", context)
 
 def post_delete(request, id =None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, id=id)
     instance.delete()
     messages.success(request, "Successfully deleted")
